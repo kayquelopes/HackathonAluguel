1aa45b59ad6d96769a479c7518435529ab143d3a
Arthur-de-Souza-Machado <arthursouzamachado2703@gmail.com>
2024-09-25 21:00:46 -0300

    feat: Update login #5

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import loginView from '@/views/loginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: loginView
     },
   ]
 })
