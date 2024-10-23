3ec5b132e402e2cbd804e57c4f164b5aa2715c25
Rafabitte1 <rafabitte1@gmail.com>
2024-10-23 16:10:10 -0300

    feat: Criação da page #7

---
+++ src/components/pagAdm.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+<template>
+  <main>
+    <div class="container-user">
+      <div class="container-item1">
+        <div class="item1-1"><img src="../../public/avatars-000424332303-cag0qn-t240x240.jpg" alt="" /></div>
+      </div>
+      <div class="container-item2">
+        <div class="item2-1">
+          <p>Nome: Daniela fas kon selos Cula</p>
+        </div>  
+        <div class="item2-3">
+          <p>Telefone: +55 (47) 0800 192 6969</p>
+          <p>País: Brasil | Estado: Rio De Janeiro</p>
+          <p>Email: Danielafaskon@gmail.com</p>
+          <p>senha: 123456789</p>
+        </div>
+      </div>
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/admView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import pagAdm from '@/components/pagAdm.vue';
+</script>
+
+<template>
+  <main>
+    <pagAdm />
+  </main>
+</template>
