1f0e9f471fcbb92217bc48aa9bad2db449012f11
jeffreynicolas <jeffreynicolas.deus@gmail.com>
2024-10-23 15:48:01 -0300

    feat: update page #10

--- src/components/ListaDeUser.vue
+++ src/components/ListaDeUser.vue
@@ -249,6 +249,47 @@
     </div>
   </main>
 </template>
+.titulo {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+h1 {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.profile-card {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  margin: 20px;
+}
+.user-img img {
+  width: 100px;
+  margin-right: 15px;
+}
+.user-info h2 {
+  margin: 0;
+  color: black;
+}
+
+.user-info p {
+  color: black;
+}
 
 <style scoped>
 </style>
